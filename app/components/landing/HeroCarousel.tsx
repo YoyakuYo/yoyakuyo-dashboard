eeebb8a38573986817cbe77a3652e0554fbb2dce
Omar Sow <omarsowbarca45@gmail.com>
2025-12-02 19:45:26 +0900

    Update hero carousel: add provided images and change interval to 3 seconds

--- app/components/landing/HeroCarousel.tsx
+++ app/components/landing/HeroCarousel.tsx
@@ -4,14 +4,29 @@ import React, { useState, useEffect } from 'react';
 import Image from 'next/image';
 import { useTranslations } from 'next-intl';
 
-// Japanese night scene images for the carousel
+// Hero background images for the carousel
 const heroImages = [
-  'https://source.unsplash.com/1920x1080/?tokyo-night-street-lights',
-  'https://source.unsplash.com/1920x1080/?japanese-izakaya-alley-night',
-  'https://source.unsplash.com/1920x1080/?japanese-hotel-night-exterior',
-  'https://source.unsplash.com/1920x1080/?japanese-onsen-night-traditional',
-  'https://source.unsplash.com/1920x1080/?japanese-countryside-night-mountain',
-  'https://source.unsplash.com/1920x1080/?osaka-night-neon-lights',
+  'https://tse3.mm.bing.net/th/id/OIP.zoOE3F889IVVuzwS_A2EiwHaEl?rs=1&pid=ImgDetMain&o=7&rm=3',
+  'https://www.liveenhanced.com/wp-content/uploads/2019/09/dental-clinic-25.jpg',
+  'https://mos-kapital.ru/wp-content/uploads/2020/11/0_90097300_1590788955_img.jpg',
+  'https://static.vecteezy.com/system/resources/previews/024/284/257/original/beauty-salon-logo-design-free-vector.jpg',
+  'https://images.arigatotravel.com/wp-content/uploads/2023/08/28145827/IshibekojiMuanRoom.png',
+  'https://th.bing.com/th/id/OIP.CTbrn8GyukLkRohqlba5JQHaEg?o=7rm=3&rs=1&pid=ImgDetMain&o=7&rm=3',
+  'https://cdn-cf.ipsy.com/contentAsset/image/d0b0b823-4c8f-4a92-965f-d9e0d1fa0148/fileAsset?byInode=1',
+  'https://i.pinimg.com/originals/92/7e/14/927e1476792755ed422bc0ee3192f8d5.jpg',
+  'https://static.vecteezy.com/system/resources/previews/023/136/451/large_2x/beauty-spa-treatment-and-relax-concept-hot-stone-massage-setting-lit-by-candles-neural-network-ai-generated-photo.jpg',
+  'https://cdn-ak.f.st-hatena.com/images/fotolife/m/massageyherapy/20200423/20200423205855.jpg',
+  'https://i.pinimg.com/originals/95/13/f2/9513f242d2e3bcd78175da692cf06b26.jpg',
+  'https://www.leisureopportunities.co.uk/images/imagesX/HIGH339960_164070_175138.jpg',
+  'https://i.etsystatic.com/37266898/r/il/49da1a/4182986944/il_1080xN.4182986944_1c19.jpg',
+  'https://tse4.mm.bing.net/th/id/OIP.dQIF1nsR84smATszJLMvTQHaEK?rs=1&pid=ImgDetMain&o=7&rm=3',
+  'https://tse3.mm.bing.net/th/id/OIP.dks9st7wuhq6v9EBOtMAlQAAAA?rs=1&pid=ImgDetMain&o=7&rm=3',
+  'https://old-tokyo.info/wp-content/uploads/2018/08/Izakaya-Goupachi-Rappongi-1-1140x641.jpg',
+  'https://images.says.com/uploads/story_source/source_image/838759/f93a.jpg',
+  'https://tblg.k-img.com/restaurant/images/Rvw/85519/640x640_rect_85519070.jpg',
+  'https://tse2.mm.bing.net/th/id/OIP.xc8VBBggd7A2F-uyS2ZU5QHaE7?rs=1&pid=ImgDetMain&o=7&rm=3',
+  'https://media.cntraveler.com/photos/5a931002d363c34048b35bf5/16:9/w_2560,c_limit/Motoyoshi_2018__DSC5176.jpg',
+  'https://jw-webmagazine.com/wp-content/uploads/2023/05/FineDiningJapan-LucaFantin-min.jpg',
 ];
 
 export default function HeroCarousel() {
@@ -21,7 +36,7 @@ export default function HeroCarousel() {
   useEffect(() => {
     const interval = setInterval(() => {
       setCurrentIndex((prev) => (prev + 1) % heroImages.length);
-    }, 5000); // Change image every 5 seconds
+    }, 3000); // Change image every 3 seconds
 
     return () => clearInterval(interval);
   }, []);
